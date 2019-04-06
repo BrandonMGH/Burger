1eb5a8618ddeb96df74b949f492d89409a706de7
Brandon Harris <BrandonMGH@Yahoo.com>
2019-04-05 19:56:52 -0700

    Homework Update

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const exphbs  = require('express-handlebars');
+
+const PORT = process.env.PORT || 8080;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const burgerController = require("./controllers/burgersController");
+app.use(burgerController);
+
+
+app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`));
